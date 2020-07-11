a08b4f593966730aa0411742bf66189af30ed602
“Muhammad <muhammad.rahmatullah@mekari.com>
2020-07-12 03:35:11 +0700

    add get header function to generate signature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  def get_headers
+    timestamp = Time.now.to_i.to_s
+    headers = {
+      "X-signature": Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest::Digest.new('sha256'),  ENV['BPJS_PASSWORD'], "#{ENV['BPJS_ID']}&#{timestamp}")).strip,
+      "X-cons-id": ENV['BPJS_ID'],
+      "X-timestamp": timestamp,
+    }
+  end
 end
